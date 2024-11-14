64a1ba1ddd57bc2f315c05f08d1c0a6ad22d101e
Jin <jyi2cute@gmail.com>
2024-11-14 08:42:43 -0500

    added in the code file for the assignment

---
+++ Homework_20_Questions_Code/Homework_20_Questions_Code/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Homework_20_Questions_Code
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the 20 Questions Game!");
+        }
+    }
+}
